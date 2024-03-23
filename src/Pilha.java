1eb3ef879d84f7c1e464b2f31cb469e493e96929
GuK4STR0 <gustavo.castro01@fatec.sp.gov.br>
2024-03-23 09:42:41 -0300

    Remoção do metodo topo

--- src/Pilha.java
+++ src/Pilha.java
@@ -11,8 +11,7 @@ public class Pilha {
             System.out.println("\nSelecione uma opção:");
             System.out.println("1. Empilhar");
             System.out.println("2. Desempilhar");
-            System.out.println("3. Exibir topo");
-            System.out.println("4. Exibir toda a pilha");
+            System.out.println("3. Exibir toda a pilha");
             System.out.println("0. Sair");
             System.out.print("Opção: ");
             opcao = scanner.nextInt();
@@ -29,18 +28,10 @@ public class Pilha {
                         int topo = pilha.pop();
                         System.out.println("Valor desempilhado: " + topo);
                     } else {
-                        System.out.println("A pilha está vazia, não é possível desempilhar.");
+                        System.out.println("A pilha vazia, não é possível desempilhar.");
                     }
                     break;
                 case 3:
-                    if (!pilha.isEmpty()) {
-                        int topo = pilha.peek();
-                        System.out.println("Topo da pilha: " + topo);
-                    } else {
-                        System.out.println("A pilha está vazia.");
-                    }
-                    break;
-                case 4:
                     if (!pilha.isEmpty()) {
                         System.out.println("Conteúdo da pilha:");
                         for (int i = pilha.size() - 1; i >= 0; i--) {
